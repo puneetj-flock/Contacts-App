ce942d603ce141dfef0dab2364e32056758222d6
puneetj-flock <puneetj@flock.com>
2022-01-17 17:43:56 +0530

    Front-End-Layout

---
+++ contact-app/client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="search-bar">
+        <input placeholder="Search Contact" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
