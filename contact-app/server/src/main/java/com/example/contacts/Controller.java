72bcfcbe222a96a594e88beef3e757fdfab36708
puneetj-flock <puneetj@flock.com>
2022-01-18 11:28:18 +0530

    API's added except update

--- contact-app/server/src/main/java/com/example/contacts/Controller.java
+++ contact-app/server/src/main/java/com/example/contacts/Controller.java
@@ -1,6 +1,8 @@
 package com.example.contacts;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,14 +14,20 @@ public class Controller {
     @Autowired
     JdbcRepo jdbcRepo;
 
+    @GetMapping("/")
+    public String check()
+    {
+        return "Hello";
+    }
+
     @PostMapping("/addContact")
     public void addContact(@RequestBody ContactDetails contact) { jdbcRepo.insert(contact); }
 
-    @PostMapping("/getContacts")
-    public void getContact(@RequestBody String name) { return jdbcRepo.get(name); }
+//    @PostMapping("/getContacts")
+//    public void getContact(@RequestBody String name) { return jdbcRepo.get(name); }
 
-    @PostMapping("/deleteContact")
-    public void deleteContact(@RequestBody String contactID) { jdbcRepo.delete(contactID); }
+    @DeleteMapping("/deleteContact")
+    public void deleteContact(@RequestBody ContactDetails id) { jdbcRepo.delete(id); }
 
     @PostMapping("/updateContact")
     public void updateContact(@RequestBody ContactDetails contact) { jdbcRepo.update(contact); }
--- contact-app/server/src/main/java/com/example/contacts/JdbcRepo.java
+++ contact-app/server/src/main/java/com/example/contacts/JdbcRepo.java
@@ -1,4 +1,43 @@
 package com.example.contacts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class JdbcRepo {
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    public void insert(ContactDetails contact)
+    {
+        BeanPropertySqlParameterSource paramSource = new BeanPropertySqlParameterSource(contact);
+        String sql_insert = "insert into contacts(id, name, email, score)  VALUES(:id, :name, :email, :score)";
+        jdbcTemplate.update(sql_insert, paramSource);
+    }
+
+    public void update(ContactDetails contact)
+    {
+        BeanPropertySqlParameterSource paramSource = new BeanPropertySqlParameterSource(contact);
+        String sql_update = "update contacts SET id = :id, name = :name, email = :email,score = :score where id = :id ";
+        jdbcTemplate.update(sql_update, paramSource);
+    }
+
+    public void delete(ContactDetails contactID)
+    {
+//        BeanPropertySqlParameterSource paramSource = new BeanPropertySqlParameterSource(contact);
+        Map params = new HashMap<>();
+        params.put("id", contactID.getId());
+
+        System.out.println("Printed here" + contactID);
+
+        String sql_delete = "delete from contacts where id = :id ";
+        jdbcTemplate.update(sql_delete, params);
+    }
 }
